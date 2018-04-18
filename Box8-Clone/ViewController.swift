e8f4e09030c3e2ec52d1f919586c31f8429285f0
Amit Kumar <amit.kumar@novanet.net>
2018-04-18 22:05:22 +0530

    :point_right: updated constraint for main storyboard :point_right: added action to show menu

--- Box8-Clone/ViewController.swift
+++ Box8-Clone/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showMenuItems(_ sender: Any) {
+        
+    }
+    
 }
 
