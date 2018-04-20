c39b308d447339397bb0aec3b47363b3084b9293
Amit Kumar <amit.kumar@novanet.net>
2018-04-20 22:44:35 +0530

    :point_right: added bottom view :point_right: removed bugs from scrollview :point_right: minor changes :point_right: added some images

--- Box8-Clone/ViewController.swift
+++ Box8-Clone/ViewController.swift
@@ -8,36 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIScrollViewDelegate {
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var menuHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var menuWidthConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var pageControl : UIPageControl!
+    @IBOutlet weak var scrollView : UIScrollView!
+    
+    var images = ["0","1","2","3","4","5","6","7","8","9","10","11"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
     var isHamburgerShown = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollViewWidth.constant = self.view.frame.size.width
+        pageControl.numberOfPages = images.count
+       
+      
+
+        scrollViewHeight.constant = 248
+        scrollView.delegate = self
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        
+        super.viewDidLayoutSubviews()
+        
+        for index in 0..<images.count
+        {
+            frame.origin.x = (scrollViewWidth.constant  * CGFloat(index)) + 10 //scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            frame.size = CGSize(width: scrollViewWidth.constant - 20 , height: scrollView.frame.size.height)
+            
+            let imageView = UIImageView(frame: frame)
+            imageView.image = UIImage(named: images[index])
+            imageView.layer.cornerRadius = 8
+            imageView.clipsToBounds = true
+            scrollView.addSubview(imageView)
+        }
+        scrollView.contentSize = CGSize(width: scrollViewWidth.constant * CGFloat(images.count), height: scrollView.frame.size.height)
+        //self.imageView.frame = self.view.bounds
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
     }
-
+    
     @IBAction func showMenuItems(_ sender: Any) {
         
         if !isHamburgerShown
         {
             leadingConstraint.constant = 250
             trailingConstraint.constant = -250
+            mainView.alpha = 0.5
             isHamburgerShown = !isHamburgerShown
+            
         }
         else{
             leadingConstraint.constant = 0
             trailingConstraint.constant = 0
+            mainView.alpha = 1.0
             isHamburgerShown = !isHamburgerShown
         }
         
