ef2de967d7a88684bfda2b9df15b7b8b953fb28d
Amit Kumar <amit.kumar@novanet.net>
2018-04-21 13:53:17 +0530

    --> Added new file UICustomCollectionViewLayout ,CustomCatogariesCell --> set cell type to custom in Storyboard in collection view cell

---
+++ Box8-Clone/View/CustomCatogariesCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomCatogariesCell.swift
+//  Box8-Clone
+//
+//  Created by Amit on 21/04/18.
+//  Copyright © 2018 Novanet. All rights reserved.
+//
+
+import UIKit
+
+class CustomCatogariesCell: UICollectionViewCell {
+    
+    @IBOutlet weak var catogeryImage: UIImageView!
+    @IBOutlet weak var catogeryName: UILabel!
+    
+    
+    var identifier = "customCell"
+    
+}
+
+
+public protocol CustomLayoutDelegate
+{
+      func collectionView(_ collectionView: UICollectionView, heightForItemAt indexPath: IndexPath, with width: CGFloat) -> CGFloat
+}
